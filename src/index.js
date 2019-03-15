09b8d02dff0752185ae652fb6cd0a93155286774
Emily Wagner <emilywagner598@gmail.com>
2019-03-15 11:12:44 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
-
-// YOUR CODE HERE
+const addBtn = document.querySelector('#new-toy-btn');
+const toyForm = document.querySelector('.container');
+const toyCollection = document.getElementById("toy-collection");
+let addToy = false;
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -16,4 +15,44 @@ addBtn.addEventListener('click', () => {
 })
 
 
-// OR HERE!
+//ask tcf about structure/contents of response
+fetch("http://localhost:3000/toys")
+.then(response => response.json())
+.then(json => {
+  addAllToToyCollection(json);
+});
+
+function addAllToToyCollection (toys) {
+  for (const toy of toys) {
+    makeToyCard(toy);
+  } 
+}
+
+function makeToyCard(toy) {
+  let id = toy["id"];
+  let name = toy["name"];
+  let image = toy["image"];
+  let likes = toy["likes"];
+
+  let cardDiv = document.createElement("div");
+  let cardH2 = document.createElement("h2");
+  let cardImg = document.createElement("img");
+  let cardP = document.createElement("p");
+  let cardButton = document.createElement("button");
+
+  cardDiv.className = "card";
+  cardImg.className = "toy-avatar";
+  cardButton.className = "like-btn";
+
+  cardDiv.appendChild(cardH2);
+  cardDiv.appendChild(cardImg);
+  cardDiv.appendChild(cardP);
+  cardDiv.appendChild(cardButton);
+
+  cardH2.textContent = name;
+  cardImg.setAttribute("src", image);
+  cardP.textContent = `${likes} likes`;
+  cardButton.textContent = "Like <3";
+
+  toyCollection.appendChild(cardDiv);
+}
