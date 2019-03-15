bbe3f6ed1690af90e03f4d7ca32cc4fdc29e7f84
halented <hallie.dunn1@gmail.com>
2019-03-15 10:17:09 -0700

    steps 1 &2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +14,38 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+const toys = document.getElementById('toy-collection')
+const TOYS_URL = "http://localhost:3000/toys"
+let toyData = []
 
-// OR HERE!
+fetch(TOYS_URL)
+  .then(response => response.json())
+  .then(json => {
+    toyData = json
+    toyData.forEach(function(toy) {
+      createToyCard(toy)
+    })
+  })
+
+function createToyCard(data){
+  let cardDiv = document.createElement('div')
+  cardDiv.className = "card"
+  let h2 = document.createElement('h2')
+  h2.innerText = data.name
+  let img = document.createElement('img')
+  img.className = "toy-avatar"
+  img.setAttribute('src', data.image)
+  let p = document.createElement('p')
+  p.innerText = `${data.likes} Likes`
+  let button = document.createElement('button')
+  button.className = 'like-btn'
+  button.innerText = "Like <3"
+
+
+  cardDiv.appendChild(h2)
+  cardDiv.appendChild(img)
+  cardDiv.appendChild(p)
+  cardDiv.appendChild(button)
+  toys.appendChild(cardDiv)
+}
+
