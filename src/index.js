ec9fa0a62a8a6a67fe00c81b32c3ac4bfcca5289
github username <crishanks12@gmail.com>
2019-03-17 14:02:03 -0700

    create and append elements, fetch initial toy cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+const TOYURL = "http://localhost:3000/toys";
 
 // YOUR CODE HERE
 
@@ -17,3 +18,37 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+fetch(TOYURL)
+.then(result => {
+  return result.json();
+})
+.then(json => {
+  addNewToy(json);
+})
+
+function addNewToy(data) {
+  for (toy of data) {
+    //Create Elements
+    const container = document.getElementById('toy-collection');
+    const div = document.createElement('div');
+    div.setAttribute('class', 'card');
+    const h2 = document.createElement('h2');
+    h2.innerText = toy.name;
+    const img = document.createElement('img');
+    img.setAttribute('class', 'toy-avatar');
+    img.src = toy.image;
+    const p = document.createElement('p');
+    p.innerText = `${toy.likes} Likes`;
+    const likeButton = document.createElement('button');
+    likeButton.setAttribute('class', 'like-btn');
+    likeButton.innerText = "Like <3";
+
+    //Append Elements
+    container.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(likeButton);
+  }
+}
